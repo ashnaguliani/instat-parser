670fcf2630c869d7d9d5236c9c3a6c217cf898b6
Ashna Guliani <ashnaguliani@users.noreply.github.com>
2020-12-13 00:00:46 -0800

    consolidated parsers into one file, removed player_code to player_ID conversion for easier QA

--- helper.py
+++ helper.py
@@ -4,10 +4,11 @@ def convert_seconds_to_minutes(seconds):
 
 def convert_code_to_playerID(code, players_table):
 	instat_name = code.split(" ", 1)[1]
-	player_IDs = players_table.loc[players_table.instat_name==instat_name, 'player_ID']
+	# player_IDs = players_table.loc[players_table.instat_name==instat_name, 'player_ID']
 
-	if len(player_IDs) != 1: 
-		print("Couldn't find '%s' in players table CSV file" % instat_name)
-		return "null"
-	else:
-		return player_IDs.values[0]
+	# if len(player_IDs) != 1: 
+	# 	print("Couldn't find '%s' in players table CSV file" % instat_name)
+	# 	return "null"
+	# else:
+	# 	return player_IDs.values[0]
+	return instat_name
---
+++ instat_parser.py
@@ -0,0 +1,100 @@
+import xml.etree.ElementTree as ET
+import csv
+import pandas as pd
+import sys
+import time
+
+from helper import convert_seconds_to_minutes, convert_code_to_playerID
+from passes import passes
+from shots import shots
+from goals import goals
+from key_events import key_events
+
+# open the XML file - uses the first argument from command line as filename
+xml_file_name = sys.argv[1]
+tree = ET.parse(xml_file_name)
+root = tree.getroot()
+
+# open a file for writing - named the same as the XML file appended with -parsed
+# output_file_name = xml_file_name.split('.')[0] + "-parsed.csv"
+# game_data = open(output_file_name, "w")
+
+# write to the file
+# csvwriter = csv.writer(game_data)
+# with game_data:
+xml_data = []
+
+#the first row of the CSV file (column names) -- list of lists is CSV writer syntax (each list is a row)
+# headers = ["ID", "start", "end", "code", "team", "action", "half", "pos_x", "pos_y"]
+
+# we want to look through each "instance" in the XML file
+for elem in tree.findall('.//instance'):
+
+# create an empty list to store all the data for this instance
+	instance = []
+	instance.append(float(elem.find('start').text))
+	instance.append(float(elem.find('end').text))
+	instance.append(elem.find('code').text)
+
+	# there's a couple instances that don't have every variable - here we'll check that the
+	# instance does have at least one of them (so we'll assume it has all the rest) to make 
+	# sure that doing a get is safe
+	if elem.find('label') is not None:
+		instance.append(elem[4][1].text)
+		instance.append(elem[5][1].text)
+		instance.append(elem[6][1].text)
+		instance.append(float(elem.find('pos_x').text))
+		instance.append(float(elem.find('pos_y').text))
+
+	# write the row to the file
+	xml_data.append(instance)
+
+events_df = pd.DataFrame(xml_data, columns = ["start", "end", "code", "team", "action", "half", "pos_x", "pos_y"]) 
+
+# last highest ID in events table - so that the IDs don't overlap with what's already in the DB
+index_to_increase = 0 #sys.argv[2]
+events_df.index += index_to_increase
+
+# open the players table CSV file with all the players' names & IDs
+players_table = pd.read_csv('players_table.csv')
+
+# the times in the second half in instat are kinda off - here we calculate what the delay is 
+second_half_delay_seconds = events_df.loc[events_df['half'] == '2nd half'].iloc[0]['start'] - 45*60
+
+# removing the weird 'start' events in each half
+events_df = events_df[events_df.code != 'Start']
+
+# placeholder lists for each of our new columns
+start_time = []
+end_time = []
+player_ID = []
+
+# iterate through each row in the events DF -
+# if the event is the the first half - leave the time as-is
+# if the event is the the second half, subtract the time delay we calculated earlier
+# also find the correct player_ID for each player from the file we opened earlier 
+for index, row in events_df.iterrows():
+    to_subtract = 0
+    if row['half'] == '2nd half':
+        to_subtract = second_half_delay_seconds
+
+    start_time.append(convert_seconds_to_minutes(row['start'] - to_subtract))
+    end_time.append(convert_seconds_to_minutes(row['end'] - to_subtract))
+    player_ID.append(convert_code_to_playerID(row['code'], players_table))
+
+
+# add all of the new columns into the DF, and drop the old ones
+# doing this individually instead of in place is a lot safer
+events_df.insert(loc = 1, column = "start_time", value = start_time)
+events_df.insert(loc = 2, column = "end_time", value = end_time)
+events_df.insert(loc = 3, column = "player_ID", value = player_ID)
+events_df.drop(['start', 'end', 'code'], axis=1, inplace=True)
+
+# output the DF into a file which is the original filename + events
+events_df.to_csv(xml_file_name.split('.')[0] + "-events.csv")
+
+# run each of the other scripts & output into their own files
+passes(pass_df = events_df.copy()).to_csv(xml_file_name.split('.')[0] + "-passes.csv") 
+shots(shot_df = events_df.copy()).to_csv(xml_file_name.split('.')[0] + "-shots.csv") 
+goals(goal_df = events_df.copy()).to_csv(xml_file_name.split('.')[0] + "-goals.csv") 
+key_events(key_event_df = events_df.copy()).to_csv(xml_file_name.split('.')[0] + "-key-events.csv") 
